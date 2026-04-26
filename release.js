0c56308065849f2d82f1e8ac5040aab686324550
Edgard <edgardsouza176@gmail.com>
2026-04-26 14:19:53 -0300

    update release

--- release.js
+++ release.js
@@ -10,7 +10,6 @@ function run(cmd) {
 function exists(file) {
     return fs.existsSync(path.join(__dirname, file))
 }
-
 try {
     run("gh auth status")
 
@@ -31,7 +30,7 @@ try {
         `dist/latest.yml`,
         `dist/Maxify-${pkg.version}-setup.exe`,
         `dist/Maxify-${pkg.version}-setup.exe.blockmap`,
-    ] 
+    ]
 
     const files = distFiles
         .filter(exists)
@@ -42,8 +41,36 @@ try {
         throw new Error("Nenhum arquivo encontrado na pasta dist.")
     }
 
+    const releaseNotes = `
+# 🚀 Maxify ${version}
+
+![Maxify Banner](https://cdn.discordapp.com/attachments/1331625775803273297/1496024486770774116/ChatGPT_Image_21_de_abr._de_2026_02_46_00.png?ex=69eef860&is=69eda6e0&hm=d8dbf442e848b27de44a7acb13dbeaa8186cbc1d0e7c73c7d7fb338e5dd016a7&)
+
+## ✨ O que há de novo
+- Interface mais moderna e fluida
+- Melhorias no sistema de otimização
+- Correções de bugs internos
+- Mais estabilidade no app
+- Melhor desempenho geral
+
+## 🛠️ Melhorias
+- Atualização automática aprimorada
+- Build mais seguro
+- Arquivos otimizados para instalação
+- Melhor compatibilidade com Windows
+
+## 💙 Obrigado por usar o Maxify
+O Maxify continua evoluindo para entregar mais desempenho, praticidade e controle para o seu PC.
+
+---
+
+### 📥 Baixe a nova versão nos arquivos abaixo
+`
+
+    fs.writeFileSync("release-notes.md", releaseNotes)
+
     run(
-        `gh release create ${version} ${files} --title "${version}" --notes "Nova atualização do Maxify" --latest`
+        `gh release create ${version} ${files} --title "🚀 Maxify ${version} - Nova versão disponível" --notes-file release-notes.md --latest`
     )
 
     console.log("\n✅ Release publicada com sucesso!")
